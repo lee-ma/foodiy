f110870b944a91bcd465af999154e347419caeb6
rocky1638 <rockzhou15@gmail.com>
2018-02-26 15:27:00 -0500

    added navbar template

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 import Home from './Home';
+import Navbar from './Navbar';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <div>
+          <Navbar />
           <Route path="/" component={Home} />
         </div>
       </BrowserRouter>
---
+++ client/src/Navbar.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Navbar extends Component {
+  render() {
+    return <div className="nav">{/*HOW TO NAVBAR*/}</div>;
+  }
+}
+
+export default Navbar;
