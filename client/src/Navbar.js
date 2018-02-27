19e8e74316b5f96c077ecbc823f3fb50ce50ce79
leema <leema9321@gmail.com>
2018-02-27 18:44:48 -0500

    Started on navbar and added proxy

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -1,8 +1,33 @@
 import React, { Component } from 'react';
 
 class Navbar extends Component {
+  componentDidMount() {
+    //TODO: Use redux to change navbar
+  }
   render() {
-    return <div className="nav">{/*HOW TO NAVBAR*/}</div>;
+    return (
+      <nav className="navbar navbar-toggleable-md navbar-light bg-red" style={{backgroundColor: "red"}}>
+        <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <a className="navbar-brand" href="/">Brand</a>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <a className="nav-link" href="#">New Recipe</a>
+            </li>
+          </ul>
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <a className="nav-link" href="#">Login</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Sign up</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    )
   }
 }
 
