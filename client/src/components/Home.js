2e384b347401e90ef5656299c5de1507a5087052
rocky1638 <rockzhou15@gmail.com>
2018-02-27 20:15:15 -0500

    added homepage login buttons

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -4,9 +4,28 @@ class Home extends Component {
   render() {
     return (
       <div className="container-fluid">
-        <div className="row">
-          <div className="col-xs-12 col-sm-10 col-sm-offset-1">
-            <h1 className="cursive">Name tbd</h1>
+        <div className="jumbotron-fluid text-align-center">
+          <div className="row">
+            <div className="col-xs-12 col-sm-5 col-sm-offset-1">
+              <h1>FOODIY</h1>
+            </div>
+            <div className="col-xs-12 col-sm-5">
+              <a className="btn btn-danger" href="/auth/google">
+                Sign in with Google
+              </a>
+              <br />
+              <a className="btn btn-primary" href="#">
+                Sign in with Facebook
+              </a>
+              <br />
+              <a className="btn" href="#">
+                Sign in with email
+              </a>
+              <hr />
+              <a className="btn" href="#">
+                Sign up with email
+              </a>
+            </div>
           </div>
         </div>
       </div>
