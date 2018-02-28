e263d910eaeb5dff301910a9bf029bd6a0f7261f
rocky1638 <rockzhou15@gmail.com>
2018-02-27 19:54:11 -0500

    added redux

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchUser } from '../actions';
+
+class Navbar extends Component {
+  componentDidMount() {
+    this.props.fetchUser();
+  }
+  render() {
+    return (
+      <nav
+        className="navbar navbar-toggleable-md navbar-light bg-red"
+        style={{ backgroundColor: 'red' }}
+      >
+        <button
+          className="navbar-toggler navbar-toggler-right"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+        <a className="navbar-brand" href="/">
+          Brand
+        </a>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                New Recipe
+              </a>
+            </li>
+          </ul>
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Login
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Sign up
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    user: state.auth
+  };
+}
+
+export default connect(mapStateToProps, { fetchUser })(Navbar);
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,18 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
 
-import App from './App';
+import reducers from './reducers';
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+import App from './components/App';
+
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector('#root')
+);
