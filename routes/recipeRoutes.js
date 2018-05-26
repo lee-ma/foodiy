6d9a3b25daacc3dfb9eae45fc2672e8afeeaaddc
rocky1638 <rockzhou15@gmail.com>
2018-05-26 19:23:44 -0400

    recipe upload now works completely

--- routes/recipeRoutes.js
+++ routes/recipeRoutes.js
@@ -4,17 +4,25 @@ const Recipe = mongoose.model('recipe');
 
 module.exports = app => {
   app.put('/api/recipes', (req, res) => {
+
+    // Construct the recipe that'll be sent up
     const newRecipe = new Recipe(
       {
         ...req.body,
         createdAt: Date.now(),
-        author: req.user,
+        author: req.user._id,
         comments: [],
         rating: null
       }
     )
-    newRecipe
-    .save()
+
+    // Construct the user's new recipe field
+    let updatedUserRecipes = { recipes: req.user.recipes.concat(newRecipe._id) }
+
+    // Update everything on Mongo
+    const promise1 = newRecipe.save()
+    const promise2 = User.findByIdAndUpdate(req.user._id, updatedUserRecipes)
+    Promise.all([promise1, promise2])
     .then()
     .catch(err => console.log(err))
   })
