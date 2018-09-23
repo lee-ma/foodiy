eaed68e3424aee7f376f297ff60eac87ed5a1224
rocky1638 <rockzhou15@gmail.com>
2018-09-23 13:01:44 -0400

    404 page done

--- client/src/scenes/NotFound.js
+++ client/src/scenes/NotFound.js
@@ -1,8 +1,31 @@
 import React from "react"
+import styled from 'styled-components'
 import { Text } from 'components'
 
+const StyledImage = styled('img')`
+  max-height: 80vh;
+  @media (max-width: 767px) {
+    margin-top: 90px;
+  }
+  margin-top: 25px;
+`
+
 const NotFound = () => (
-  <Text>Oops.</Text>
+  <div className="row">
+    <div className="col-xs-12 col-md-8 offset-md-2">
+      <div className="text-align-center">
+        <StyledImage
+          src="https://s3.amazonaws.com/foodiy-beta/404.png"
+          className="img-fluid"
+        />
+        <Text center block big
+          semiBold>
+      Oops! The page you are looking for does not exist
+          <span role="img" aria-label="sad face"> 😢</span>
+        </Text>
+      </div>
+    </div>
+  </div>
 )
 
 export default NotFound
