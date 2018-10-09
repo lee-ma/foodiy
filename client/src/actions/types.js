3c83f00e3837640b9def954dfd53c5295b9bfc8f
lee-ma <leema9321@gmail.com>
2018-10-08 22:16:42 -0400

    remove unnecessary redux

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,6 +1,5 @@
 export const FETCH_USER = "fetch_user"
 export const UPDATE_USER = "update_user"
-export const CREATE_USER = "create_user"
 
 export const ADD_RECIPE = "add_recipe"
 export const FETCH_RECIPES = "fetch_recipes"
